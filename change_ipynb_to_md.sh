158039f08bef2f3ef66adae42e7f5c24c8c321cb
blackpigseo <wdseo@lsitc.com>
2021-09-03 10:22:10 +0900

    이미지 업로드 확인

--- change_ipynb_to_md.sh
+++ change_ipynb_to_md.sh
@@ -1,11 +1,12 @@
 #!/bin/bash/
 # 전체 경로명이 아니라 확장자를 제외한 파일명만 입력할 것 
 
-input=$1
-file_name=${input%%.*}
+file_path=$1
+file_path=${file_path#*./_notebooks}
+input=${file_path%%.*}
 echo ${file_name}
 function converter() {
-    jupyter nbconvert --to markdown `pwd`/_notebooks/${file_name} --output-dir=`pwd`/_posts
+    jupyter nbconvert --to markdown `pwd`/_notebooks/${input} --output-dir=`pwd`/_posts --ExtractOutputPreprocessor.enabled=False
     echo "Success"
 }
 converter
